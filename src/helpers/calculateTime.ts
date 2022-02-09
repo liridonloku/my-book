1e1535c575ca5280283766e9adf41ead78aa1191
Liridon Loku <liridonloku@gmail.com>
2022-02-09 16:01:03 +0100

    Add function to calculate time passed since post/comment

---
+++ src/helpers/calculateTime.ts
@@ -0,0 +1,43 @@
+const calculateTime = (postDate: Date, now: Date): string => {
+  let differenceInMinutes = (now.getTime() - postDate.getTime()) / 1000 / 60;
+  // Years
+  if (differenceInMinutes / (60 * 24 * 365) >= 1) {
+    return `${Math.floor(differenceInMinutes / (60 * 24 * 365))} year${
+      Math.floor(differenceInMinutes / (60 * 24 * 365)) === 1 ? "" : "s"
+    } ago`;
+  }
+  // Months
+  if (differenceInMinutes / (60 * 24 * 30) >= 1) {
+    return `${Math.floor(differenceInMinutes / (60 * 24 * 30))} month${
+      Math.floor(differenceInMinutes / (60 * 24 * 30)) === 1 ? "" : "s"
+    }  ago`;
+  }
+  // Weeks
+  if (differenceInMinutes / (60 * 24 * 7) >= 1) {
+    return `${Math.floor(differenceInMinutes / (60 * 24 * 7))} week${
+      Math.floor(differenceInMinutes / (60 * 24 * 7)) === 1 ? "" : "s"
+    }  ago`;
+  }
+  // Days
+  if (differenceInMinutes / (60 * 24) >= 1) {
+    return `${Math.floor(differenceInMinutes / (60 * 24))} day${
+      Math.floor(differenceInMinutes / (60 * 24)) === 1 ? "" : "s"
+    } ago`;
+  }
+  // Hours
+  if (differenceInMinutes / 60 >= 1) {
+    return `${Math.floor(differenceInMinutes / 60)} hour${
+      Math.floor(differenceInMinutes / 60) === 1 ? "" : "s"
+    }  ago`;
+  }
+  // Minutes
+  if (differenceInMinutes >= 1) {
+    return `${differenceInMinutes} minutes${
+      differenceInMinutes === 1 ? "" : "s"
+    }  ago`;
+  }
+  // Now
+  return "just now";
+};
+
+export default calculateTime;
