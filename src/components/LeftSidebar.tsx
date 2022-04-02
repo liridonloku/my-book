5ad04c49f00e84b1d3ec2837a6136c7a22134453
Liridon Loku <liridonloku@gmail.com>
2022-04-02 18:52:44 +0200

    Display user info on left sidebar

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import StyledLeftSidebar from "./styles/LeftSidebar.styled";
 import image from "../images/profile.jpg";
 import { People } from "@styled-icons/material/People";
+import { useAppSelector } from "../app/hooks";
 
 interface Props {}
 
 const LeftSidebar: React.FC<Props> = () => {
+  const user = useAppSelector((state) => state.user);
   return (
     <StyledLeftSidebar>
       <div className="profile-link">
         <div className="image">
-          <img src={image} alt="user" />
+          <img src={user.photoUrl || image} alt="img" />
         </div>
-        <p className="user-name">User Name</p>
+        <p className="user-name">{user.name}</p>
       </div>
       <div className="friends-link">
         <div className="icon">
